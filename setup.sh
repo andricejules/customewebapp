8dc805839639a68f45d6878be5753b0d7b2c5d65
andricejules <42875784+andricejules@users.noreply.github.com>
2022-04-03 17:29:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ enableCORS = false\n\
 \n\
 [theme]\n\
 primaryColor = '#E694FF'\n\
-backgroundColor = '#00172B'\n\
+backgroundColor = '#F8F0E3'\n\
 secondaryBackgroundColor = '#0083B8'\n\
 \n\
 " > ~/.streamlit/config.toml
